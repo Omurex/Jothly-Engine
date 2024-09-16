7a1b929b909c0b3ce9c6f26d751ba0e0e5bac7dc
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-09-16 03:55:15 -0400

    Cleared project

--- _build/Constants.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-
-namespace jothly::con
-{
-	static const float VECTOR_MARGIN_OF_ERROR = .00001f;
-	static std::string RESOURCE_PATH; // Set in game.cpp for now, figure out better way to do this
-}
--- cmake-build/raylib-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CodingStuff/RaylibProjects/Jothly/raylib-master
-# Build directory: C:/CodingStuff/RaylibProjects/Jothly/cmake-build/raylib-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
